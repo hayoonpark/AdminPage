868cfcfde6c453c8acea3081bfc08e463bfe0d17
hayoonpark <gkdbs0120@gmail.com>
2021-08-26 01:47:18 +0900

    Repository practice1

---
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.example.study.repository;
+
+import com.example.study.StudyApplication;
+import com.example.study.model.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+
+public class UserRepositoryTest extends StudyApplication {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void create(){
+
+        User user = new User();
+
+        user.setAccount("TestUser01");
+        user.setEmail("TestUser01@gmail.com");
+        user.setPhoneNumber("010-111-1111");
+        user.setCreatedAt(LocalDateTime.now());
+        user.setCreatedBy("admin");
+
+        User newUser = userRepository.save(user);
+        System.out.println("newUser : "+ newUser) ;
+    }
+
+    public void read(){
+
+    }
+
+    public void update(){
+
+    }
+
+    public void delete(){
+
+    }
+}
